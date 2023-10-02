ac6199070be322b4b0cd0f8935004fa3566209ce
Vanshaj Bhatnagar <vanshajbhatnagar10@gmail.com>
2023-10-02 22:48:25 +0530

    fixed UI of confirm buy window.

--- Client/src/components/Product_Details/Dialog.js
+++ Client/src/components/Product_Details/Dialog.js
@@ -75,7 +75,7 @@ const Dialog = ({ isOpen, onClose, onSave, id }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="dialog-overlay bg-gray-800 opacity-50 absolute inset-0"></div>
-      <div className="dialog-content bg-white p-8 rounded-lg shadow-lg z-50 w-1/2">
+      <div className="dialog-content bg-white p-8 rounded-lg shadow-lg z-50 w-full md:w-1/2">
         <button
           className="absolute top-2 right-2 text-gray-700"
           onClick={onClose}
@@ -84,8 +84,8 @@ const Dialog = ({ isOpen, onClose, onSave, id }) => {
         </button>
         <h2 className="text-2xl font-semibold mb-4">Update Your Details</h2>
         <form className="dialog-form">
-          <div className="flex w-full justify-between">
-            <div className="w-1/2 mr-1">
+          <div className="flex flex-col md:flex-row w-full justify-between">
+            <div className="w-full md:w-1/2 mr-1">
               <FormField
                 label="Name"
                 name="fullName"
@@ -119,7 +119,7 @@ const Dialog = ({ isOpen, onClose, onSave, id }) => {
                 error={formErrors.phoneNo}
               />
             </div>
-            <div className="w-1/2 ml-1">
+            <div className="w-full md:w-1/2 ml-1">
               <FormField
                 label="Address"
                 name="address"
@@ -158,14 +158,14 @@ const Dialog = ({ isOpen, onClose, onSave, id }) => {
             <button
               type="button"
               onClick={onClose}
-              className="w-1/4 bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-800 transition duration-300"
+              className="w-1/3 md:w-1/4 bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-800 transition duration-300"
             >
               Cancel
             </button>
             <button
               type="button"
               onClick={handleSave}
-              className="w-1/4 bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-800 transition duration-300"
+              className="w-1/3 md:w-1/4 bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-800 transition duration-300"
             >
               Confirm
             </button>
