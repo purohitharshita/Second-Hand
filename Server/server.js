f94becaa1acf77fa27f6ab93c54d037184bf6930
Vanshaj Bhatnagar <vanshajbhatnagar10@gmail.com>
2023-10-02 21:37:09 +0530

    server fix-cors

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "*",
   })
 );
 
